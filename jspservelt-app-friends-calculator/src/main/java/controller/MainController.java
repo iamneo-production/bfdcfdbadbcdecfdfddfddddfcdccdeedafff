63ab6091c35ca9120d68e707483d225e320ae31f
Iamneo <techadmin@iamneo.ai>
2023-08-24 02:24:46 +0000

    Backup Commit

--- jspservelt-app-friends-calculator/src/main/java/controller/MainController.java
+++ jspservelt-app-friends-calculator/src/main/java/controller/MainController.java
@@ -1,5 +1,8 @@
 package controller;
+
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,62 +10,66 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = {"/jspservelt-app-friends-calculator/friends"})
+@WebServlet(urlPatterns= {"/jspservelt-app-friends-calculator/friends"})
 public class MainController extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-
+	private static final long serialVersionUID = 1L;
+       
     public MainController() {
         super();
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
-        dispatcher.forward(request, response);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+    		throws ServletException, IOException {
+    	
+    	RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+		dispatcher.forward(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String myName = request.getParameter("friend1");
-        String friendName = request.getParameter("friend2");
-
-        int result = calculate(myName, friendName);
-
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+    	String myName = request.getParameter("friend1");
+    	String friendName = request.getParameter("friend2");
+    	
+        int result = calculate(myName,friendName);
+		
         String calculate = request.getParameter("calculate");
-
-        if (calculate != null) {
-            request.setAttribute("myName", myName);
-            request.setAttribute("friendName", friendName);
-            request.setAttribute("result", result);
+        
+        System.out.println(result);
+        if(calculate!=null) {
+//        	PrintWriter out = response.getWriter();
+//        	out.println("<h3>Based on the name similarity, the friendship strength between "+myName+" and "+friendName+" is: "+result+"</h3>");
+         	request.setAttribute("myName", myName);
+        	request.setAttribute("friendName", friendName);
+        	request.setAttribute("result", result);
         }
+        
+		RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+		dispatcher.forward(request, response);
+	}
+	
+	//Dont change the above lines of code
+	
+//	Start Enterinng your code here...
+public static int findSum(int num){
+	int sum = 0;
+	while(num>0){
+		sum = sum + (num%10);
+		num = num/10;
+	}
+	return sum;
+}
+public static int calculate(String myName, String friendName) {
+	int f1 = 0,f2 = 0, FRIENDS;
+	for(int i = 0;i<myName.length();i++){
+		f1 += (int)myName.charAt(i);
+	}
+	for(int i =0;i<friendName.length();i++){
+		f2 += (int)friendName.charAt(i);
+	}
 
-        RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
-        dispatcher.forward(request, response);
-    }
-
-    // Method to calculate friendship percentage
-    private int calculate(String myName, String friendName) {
-        int similarityScore = calculateSimilarity(myName, friendName);
-        int friendshipPercentage = similarityScore * 10; // Assuming a simple formula
-
-        return friendshipPercentage;
-    }
-
-    // Method to calculate similarity score (You need to implement this)
-    private int calculateSimilarity(String name1, String name2) {
-        int similarityScore = 0;
-
-        // Implement your logic to calculate similarity between two names
-        // For example, you could compare characters, calculate Levenshtein distance, etc.
-        // Return a similarity score (a higher score indicates higher similarity)
-
-        // For this example, let's just compare the characters
-        for (int i = 0; i < Math.min(name1.length(), name2.length()); i++) {
-            if (name1.charAt(i) == name2.charAt(i)) {
-                similarityScore++;
-            }
-        }
-
-        return similarityScore;
-    }
+	FRIENDS = (f1 + f2) % 100;
+	return FRIENDS;
+}
+	
 }
