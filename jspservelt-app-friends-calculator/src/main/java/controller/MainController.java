962d4f415384db11d84d539927561a81287f5914
Iamneo <techadmin@iamneo.ai>
2023-09-06 23:51:30 +0000

    Backup Commit

--- jspservelt-app-friends-calculator/src/main/java/controller/MainController.java
+++ jspservelt-app-friends-calculator/src/main/java/controller/MainController.java
@@ -63,8 +63,8 @@ public class MainController extends HttpServlet {
 public static int findSum(int num){
 	int sum = 0;
 	while(num>0){
-		sum = sum + (num%10);
-		num = num/10;
+		sum = sum + (i%10);
+		u = i/10;
 	}
 	return sum;
 }
